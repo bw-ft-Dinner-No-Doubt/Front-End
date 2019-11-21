4dccfd03a9d6f0933d299bd1dd8794e5b49d2a7e
Sladan <sladanmastilovic@gmail.com>
2019-11-21 03:02:34 -0800

    working-on-dashboard

--- dinner-no-doubt/src/App.js
+++ dinner-no-doubt/src/App.js
@@ -2,19 +2,34 @@ import React from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
-import MealWheel from './components/MealWheel';
-import RestaurantList from './components/RestaurantList';
+// import Dashboard from './components/Dashboard';
+// import MealWheel from './components/MealWheel';
+// import RestaurantList from './components/RestaurantList';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div>
+        <Switch>
+        <Route path="/" exact component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        </Switch> 
+
+        {/* <Dashboard /> */}
+     
       
-     <SignIn />
-     <RestaurantList/>
+     
+
+
+
+
+
+     {/* <RestaurantList/> */}
      {/* <MealWheel/> */}
    
     </div>
+    </Router>
   );
 }
 
---
+++ dinner-no-doubt/src/components/Dashboard.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Menu } from "semantic-ui-react";
+import { NavLink, Route } from "react-router-dom";
+import RecentHistory from "./RecentHistory";
+import MyProfile from "./MyProfile";
+
+export default function Dashboard() {
+   const [activeItem, setActiveItem] = useState('');
+   const handleItemClick = (e, { name }) => {
+      setActiveItem(name);
+   }
+
+   return (
+      <>
+         <Menu tabular>
+            <Menu.Item
+               name='RecentHistory'
+               active={activeItem === 'RecentHistory'}
+               onClick={handleItemClick}
+            >
+            <NavLink to="/RecentHistory">History</NavLink>
+        </Menu.Item>
+            <Menu.Item
+               name='MyProfile'
+               active={activeItem === 'MyProfile'}
+               onClick={handleItemClick}
+            >
+            <NavLink to="/MyProfile">MyProfile</NavLink>
+        </Menu.Item>
+            </Menu>
+            <Route path="/RecentHistory" component={RecentHistory} />
+            <Route path="/MyProfile" component={MyProfile} />
+        </>
+   )
+};
