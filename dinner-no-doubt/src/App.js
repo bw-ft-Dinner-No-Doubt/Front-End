a312e23ab6993a3366b2cbbfc046909d6613670d
juarezfrench <markedfrench@gmail.com>
2019-11-21 15:00:58 -0600

    Setup main Routes (private & login)

--- dinner-no-doubt/src/App.js
+++ dinner-no-doubt/src/App.js
@@ -1,27 +1,47 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute'
 import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
-// import Dashboard from './components/Dashboard';
+// import SignUp from './components/SignUp';
+
 // import MealWheel from './components/MealWheel';
 import RestaurantList from './components/RestaurantList';
 import './App.css';
+import Dashboard from './user/Dashboard';
 
 function App() {
   return (
     <Router>
-    <div>
-        <Switch>
-        <Route path="/" exact component={SignIn} />
-        <Route path="/signup" component={SignUp} />
-        </Switch> 
-
-        {/* <Dashboard /> */}
-  <RestaurantList/>
-     {/* <MealWheel/> */}
-   
+    <div className="App">
+      <ul>
+        <li>
+          <Link to="/login">LoginForm</Link>
+        </li>
+        <li>
+          <Link to="/protected">User Dashboard</Link>
+        </li>
+      </ul>
+      <Switch>
+        <PrivateRoute path="/protected" component={Dashboard} />
+        <Route path="/login" component={SignIn} />
+        <Route component={SignIn} />
+      </Switch>
     </div>
-    </Router>
+  </Router>
+
+  //   <Router>
+  //   <div>
+  //       <Switch>
+  //       <Route path="/" exact component={SignIn} />
+  //       <Route path="/signup" component={SignUp} />
+  //       </Switch> 
+
+  //       {/* <Dashboard /> */}
+  // <RestaurantList/>
+  //    {/* <MealWheel/> */}
+   
+  //   </div>
+  //   </Router>
   );
 }
 
