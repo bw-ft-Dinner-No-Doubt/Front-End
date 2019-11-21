7e504e7922ce4f4769bd0ab76768ec089111046f
Sladan <sladanmastilovic@gmail.com>
2019-11-21 06:29:18 -0800

    partial logic for profile card

--- dinner-no-doubt/src/components/ProfileCard.js
+++ dinner-no-doubt/src/components/ProfileCard.js
@@ -11,10 +11,15 @@ const MovieCard = props => {
             <h4>{props.profile.zipcode}</h4>
             <h4>{props.profile.foodPref}</h4>
             <h4>{props.profile.platformPref}</h4>
-
-
+            <button className='edit-btn' onClick={() => props.setUserProfile()}>Edit</button>
         </div>
     )
-}
+};
+
+// function User(props) {
+//     const [user, setUserProfile] = useState ({userName: '', email: '', address: '', city: '',
+//     state: '', zipcode: ''});
+//     const handleChange = e => {
+//         setUserProfile ({[e.target.name]: e.target.value});
 
 export default MovieCard;
