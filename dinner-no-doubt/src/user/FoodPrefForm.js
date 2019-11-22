a09f33903d3b5febb74a23e75c6a6161f32a9c12
juarezfrench <markedfrench@gmail.com>
2019-11-22 14:48:11 -0600

    Couldn't deploy - just commented out HTML

--- dinner-no-doubt/src/user/FoodPrefForm.js
+++ dinner-no-doubt/src/user/FoodPrefForm.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Form, Field, withFormik } from 'formik';
 // import * as Yup from 'yup';
 import {AxiosWithAuth} from '../components/utilities/AxiosWithAuth';
+import { string } from 'prop-types';
 
-const OnboardingForm = ({ values, errors, touched, status, setFieldValue }) => {
+const FoodPrefForm = ({ values, errors, touched, status, setFieldValue }) => {
     // const [user, setUser] = useState([]);
  
   
@@ -50,7 +51,7 @@ const OnboardingForm = ({ values, errors, touched, status, setFieldValue }) => {
              <option value="uberEats">Uber Eats</option>
              <option value="grubHub">Grub Hub</option>
             </Field> */}
-            
+   
           { console.log(values)}
           
           <button type="submit">Submit Food Preferences</button>
@@ -63,34 +64,25 @@ const OnboardingForm = ({ values, errors, touched, status, setFieldValue }) => {
  };
 
 
- const FormikOnboardingForm = withFormik({
+ const FormikFoodPrefForm = withFormik({
      
-    mapPropsToValues({ username,  password, email, streetAddress, city, state, zipcode,
-        foodPref,spicy,vegan,vegetarian, wheelchair, outdoor, woman}) {
+    mapPropsToValues({foodPref,spicy,vegan,vegetarian, wheelchair, outdoor, woman}) {
        return {
         //    foodPref: foodPref||0
-          spicy: spicy || 0,
-          vegan: vegan || 0,
-          vegetarian: vegetarian || 0, 
-          wheelchair: wheelchair || 0,
-          outdoor: outdoor || 0,
-          woman: woman || 0,
+          spicy: spicy || false,
+          vegan: vegan || false,
+          vegetarian: vegetarian || false, 
+          wheelchair: wheelchair || false,
+          outdoor: outdoor || false,
+          woman: woman || false,
         
        }
     },
  
-    // validationSchema: Yup.object().shape({
-    //    userName: Yup.string().required("USERNAME REQUIRED!"),
-    //    email: Yup.string()
-    //       .required("EMAIL REQUIRED!"),
-    //    password: Yup.string().required("PASSWORD REQUIRED!"),
-    //   //  address: Yup.string().required("ADDRESS REQUIRED!"),
-    //   //  city: Yup.string().required("CITY REQUIRED!"),
-    //   //  state: Yup.string().required("STATE REQUIRED!"),
-    //    zipcode: Yup.number().required("ZIP CODE REQUIRED!")
-    // }),
+
  
-    handleSubmit(values, { setStatus, resetForm }) {
+    handleSubmit(values) {
+       
         
       
             AxiosWithAuth()
@@ -98,24 +90,14 @@ const OnboardingForm = ({ values, errors, touched, status, setFieldValue }) => {
               .then(response => {
                 console.log(response);
               })
-              .catch(error => console.error("FoodPrefForm:",error));
+              .catch(error => console.error("FoodPrefForm:",error))
            
-            // useEffect(didUpdate, []);
+
            
      
-        
-    //    AxiosWithAuth()
-    //      .post('api/diner', values)
-    //      .then(res => {
-    //         console.log(res)
-    //         localStorage.setItem('token', res.data.token);
-    //       })
-    //       .catch(error => {
-    //          console.log(error);
-    //       })
-    }
-   
+            }
  
- })(OnboardingForm);
+ })(FoodPrefForm)
+
  
- export default FormikOnboardingForm;
+ export default FormikFoodPrefForm;
